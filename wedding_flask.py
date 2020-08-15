498df3bd29ed34729681d453dce1faab5073c578
eldho.suresh <eldho.suresh@dpc.vic.gov.au>
2020-08-16 00:09:06 +1000

    adding grand total variables

--- wedding_flask.py
+++ wedding_flask.py
@@ -13,8 +13,9 @@ def home():
         for key, value in shop_list.items():
             item_quantity = 0 if request.form[key] == '' else request.form[key]
             quantity[key] = item_quantity
-        print(quantity)
-        return render_template("receipt.html", shop_list=shop_list, quantity=quantity)
+        grand_total = 0 # call function for grand total
+        grand_total_aed = 0 # call conversion function
+        return render_template("receipt.html", shop_list=shop_list, quantity=quantity, grand_total=grand_total, grand_total_aed=grand_total_aed)
     else:
         return render_template("list.html", dict=shop_list)
 
