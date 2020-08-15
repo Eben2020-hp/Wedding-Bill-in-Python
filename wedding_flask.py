f168e9e66477953c8d6f2981a4e92d8d5bcafa76
eldho.suresh <eldho.suresh@dpc.vic.gov.au>
2020-08-15 23:31:15 +1000

    receiving back values

--- wedding_flask.py
+++ wedding_flask.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-items = {'Saree':50000, 'Gown':100000, 'Bangles': 15000, 'Shoes': 20000}
+shop_list = {'Saree':50000, 'Gown':100000, 'Bangles': 15000, 'Shoes': 20000}
+quantity = {}
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("list.html", object=items)
+    if request.method == 'POST':
+        # print(request.form['Saree'])
+        for key, value in shop_list.items():
+            quantity[key] = request.form[key]
+        print(quantity)
+    else:
+        return render_template("list.html", dict=shop_list)
 
 
 if __name__ == "__main__":
