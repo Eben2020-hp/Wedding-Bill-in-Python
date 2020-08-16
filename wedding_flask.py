4b9fa21de3e9e6656dc5a54b99e46b335b79fec2
eldho.suresh <eldho.suresh@dpc.vic.gov.au>
2020-08-16 20:01:50 +1000

    function for total price

--- wedding_flask.py
+++ wedding_flask.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 shop_list = {'Saree':50000, 'Gown':100000, 'Bangles': 15000, 'Shoes': 20000}
 quantity = {}
 
+def total_price(quantity):
+    total_price = 0
+    for key, value in quantity.items():
+        total_price += int(value) * shop_list[key]
+    return(total_price)
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -13,12 +19,12 @@ def home():
         for key, value in shop_list.items():
             item_quantity = 0 if request.form[key] == '' else request.form[key]
             quantity[key] = item_quantity
-        grand_total = 0 # call function for grand total
+        print(quantity)
+        grand_total = total_price(quantity)
         grand_total_aed = 0 # call conversion function
         return render_template("receipt.html", shop_list=shop_list, quantity=quantity, grand_total=grand_total, grand_total_aed=grand_total_aed)
     else:
         return render_template("list.html", dict=shop_list)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
